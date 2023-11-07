5959ca617c356214abfd454255ff02b9df427c37
Harithaje508 <haritha.errolla@gmail.com>
2023-11-07 19:40:32 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def haritha(request):
+    return HttpResponse('<h2><i>Kannayya i cant able to manage all things help me kannayya</i></h2>')
+def krishna(request):
+    return HttpResponse('<h2><i><marquee>I always there for u sakhi whats the point of worrying trustme thalli okay na </marquee></i></h2>')
